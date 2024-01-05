f868300d7e997b9188ac00afcac44b5906ce9db5
Vinicius <vini383@gmail.com>
2024-01-05 17:33:18 -0300

    Feat: Implemented a method to generate a random name and surname to Create new user on DB

--- Models/User.cs
+++ Models/User.cs
@@ -1,5 +1,6 @@
 
 
+using System.Text;
 using Dapper.Contrib.Extensions;
 
 namespace BlogDapper.Models
@@ -14,5 +15,45 @@ namespace BlogDapper.Models
         public string Bio { get; set; }
         public string Image { get; set; }
         public string Slug { get; set; }
+
+        public User GenerateRandomUser()
+        {
+            Random r = new Random();
+            string RandomFirstName = GenerateName(r.Next(6, 12));
+            string RandomSurName = GenerateName(r.Next(4, 16));
+
+            var user = new User();
+            user.Name = $"{RandomFirstName} {RandomSurName}";
+            user.Email = $"{RandomFirstName}@gmail.com";
+            user.PasswordHash = Convert.ToBase64String(Encoding.UTF8.GetBytes(RandomFirstName));
+            user.Bio = $"{RandomFirstName.ToUpper()}";
+            user.Image = $"https://plus.unsplash.com/{RandomFirstName}_${RandomSurName}";
+            user.Slug = $"{RandomFirstName}-{RandomSurName}";
+
+            return user;
+        }
+
+        private static string GenerateName(int len)
+        {
+            Random r = new Random();
+            string[] consonants = { "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "l", "n", "p", "q", "r", "s", "sh", "zh", "t", "v", "w", "x" };
+            string[] vowels = { "a", "e", "i", "o", "u", "ae", "y" };
+            string Name = "";
+            Name += consonants[r.Next(consonants.Length)].ToUpper();
+            Name += vowels[r.Next(vowels.Length)];
+            int b = 2; //b tells how many times a new letter has been added. It's 2 right now because the first two letters are already in the name.
+            while (b < len)
+            {
+                Name += consonants[r.Next(consonants.Length)];
+                b++;
+                Name += vowels[r.Next(vowels.Length)];
+                b++;
+            }
+
+            return Name;
+
+
+        }
+
     }
 }
