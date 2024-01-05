ed596289d7aeae9a0b2e2663afb2bcd69aedbb00
Vinicius <vini383@gmail.com>
2024-01-05 18:09:21 -0300

    feat: structure modifications on UserModel

--- Models/User.cs
+++ Models/User.cs
@@ -38,10 +38,10 @@ namespace BlogDapper.Models
             Random r = new Random();
             string[] consonants = { "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "l", "n", "p", "q", "r", "s", "sh", "zh", "t", "v", "w", "x" };
             string[] vowels = { "a", "e", "i", "o", "u", "ae", "y" };
-            string Name = "";
+            string Name = string.Empty;
             Name += consonants[r.Next(consonants.Length)].ToUpper();
             Name += vowels[r.Next(vowels.Length)];
-            int b = 2; //b tells how many times a new letter has been added. It's 2 right now because the first two letters are already in the name.
+            int b = 2;
             while (b < len)
             {
                 Name += consonants[r.Next(consonants.Length)];
